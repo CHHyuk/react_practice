d5f408471c8ab1e7f7e3a7da3c37ceca0bae0faf
CHHyuk <gksgur1038@naver.com>
2023-04-10 21:39:52 +0900

    04/10 리액트 연습

--- react-test2/src/App.js
+++ react-test2/src/App.js
@@ -1,5 +1,38 @@
+import React, { useState } from 'react'; 
+
 function App() {
-  return <div>안녕</div>;
+  
+  const onBtnIncreaseClicked = () => {
+    setNum(num + 1);
+  };
+  
+  const onBtnDecreaseClicked = () => {
+    setNum(num - 1);
+  };
+
+  const onBtnIncreaseClicked_10 = () => {
+    setNum(num + 10);
+  };
+
+  const onBtnDecreaseClicked_10 = () => {
+    setNum(num - 10);
+  };
+  const [num, setNum] = useState(0);
+
+  
+  return (
+    <>
+      NUM : {num}
+      <br></br>
+      <button onClick={onBtnIncreaseClicked}>+1</button>
+      <br></br>
+      <button onClick={onBtnDecreaseClicked}>-1</button>
+      <br></br>
+      <button onClick={onBtnIncreaseClicked_10}>+10</button>
+      <br></br>
+      <button onClick={onBtnDecreaseClicked_10}>-10</button>
+    </>
+  );
 }
 
 export default App;
