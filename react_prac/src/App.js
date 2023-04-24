cc1e476ad345561712132ff38c3876c731d366f9
CHHyuk <gksgur1038@naver.com>
2023-04-24 12:05:42 +0900

    04/24 리액트 기록,arr,map

--- react_prac/src/App.js
+++ react_prac/src/App.js
@@ -1,9 +1,14 @@
+
 import NoRecord from "./NoRecord";
 
 function App() {
+  const nums = [1,2,3,4,5,6,7,8];
+  const arr = nums.map((el) => <li>내용 {el} </li>)
   return (
     <>
       <NoRecord />
+      <hr />
+      <ul>{arr}</ul>
     </>
   );
 }
