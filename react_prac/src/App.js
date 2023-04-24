1066e4cf9fdf94b9c4016f9570a0da1f41c8fac4
CHHyuk <gksgur1038@naver.com>
2023-04-24 12:16:39 +0900

    04/24 리액트 엔터 입력 기록 v1

--- react_prac/src/App.js
+++ react_prac/src/App.js
@@ -3,7 +3,7 @@ import NoRecord from "./NoRecord";
 
 function App() {
   const nums = [1,2,3,4,5,6,7,8];
-  const arr = nums.map((el) => <li>내용 {el} </li>)
+  const arr = nums.map((el,index) => <li key={index}>내용 {el} </li>)
   return (
     <>
       <NoRecord />
--- react_prac/src/NoRecord.js
+++ react_prac/src/NoRecord.js
@@ -1,21 +1,47 @@
 import React, { useState } from "react";
 
 export default function NoRecord() {
-  const [no,setNo] = useState(0);
+  const [no,setNo] = useState('');
   const [recordedNos,setrecordedNos] = useState([]);
 
-  const saveNo = () => {
+  const saveNo = (e) => {
+    e.preventDefault();
+
+    if( no === '') {
+      alert('숫자를 입력해주세요');
+      return;
+    }
+
     setrecordedNos([...recordedNos,no]);
+    setNo('');
   };
+
+  const li = recordedNos.map((el,index) => <li key={index}>{el}</li>)
 
   return(
     <>
-      <input type="number" value={no} onChange={(e) => setNo(e.target.valueAsNumber)}/>
-      <button type="button" onClick={saveNo}>
-      기록
-      </button>
+      <form>
+        <input type="number" value={no} onChange={(e) => setNo(e.target.valueAsNumber)}/>
+        <button type="submit" onClick={saveNo}>
+        기록
+        </button>
+      </form>
+      
       <hr />
+      <h1>기록된 숫자 v1</h1>
       기록된 숫자 : [{recordedNos.join(",")}]
+      
+      <hr />
+      <h1>기록된 숫자 v2-1</h1>
+      <ul>{li}</ul>
+
+      <hr />
+      <h1>기록된 숫자 v2-2</h1>
+      <ul>
+        {recordedNos.map((el,index) => (
+          <li key={index}>{el}</li>
+        ))}
+      </ul>
     </>
   );
 }
