c77e1414708b89b069192ae12ecfe99eff87fe1f
CHHyuk <gksgur1038@naver.com>
2023-04-24 20:56:27 +0900

    04/24 데이지ui + 폰트어썸 아이콘 활용, alert 꾸미기

--- react_prac/src/App.js
+++ react_prac/src/App.js
@@ -1,9 +1,22 @@
-import RefEx from "./RefEx";
+function Alert() {
+  return (
+    <div className="alert alert-info shadow-lg">
+      <div className="text-white">
+        <span>
+          <i className="fa-solid fa-circle-info"></i>
+        </span>
+        <span>알림창 꾸미기 + 아이콘 설정</span>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <>
-      <RefEx />
+      <Alert />
+      <hr className="my-4" />
+      <Alert />
     </>
   );
 }
