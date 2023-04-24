85ad78c861d0403e641b692f9f5a32367f5b76c3
CHHyuk <gksgur1038@naver.com>
2023-04-24 21:48:06 +0900

    04/24 리액트 classnames를 활용한 액션

--- react_prac/src/App.js
+++ react_prac/src/App.js
@@ -1,22 +1,69 @@
-function Alert() {
+import React, { useState } from "react"
+import classnames from 'https://cdn.skypack.dev/classnames';
+
+let NotifyOnce__workDone = false;
+
+function NotifyOnce({ children }) {
+  const [visible, setVisible] = useState(false);
+
+  if (NotifyOnce__workDone == false) {
+    setTimeout(function () {
+      setVisible(true);
+    },1000);
+
+    setTimeout(function () {
+      setVisible(false);
+    },3000);
+
+    NotifyOnce__workDone = true;
+  }
+
+  return (
+    <div
+      className={classnames(
+        "fixed transition-all right-[10px]",
+        {
+          "top-[-60px]": !visible,
+        },
+        {
+          "top-[10px]": visible,
+        }
+      )}
+    >
+      {children}
+    </div>
+
+  )
+}
+
+function Alert({ color: _color, children }) {
+  const color = _color ?? "white";
+
   return (
     <div className="alert alert-info shadow-lg">
-      <div className="text-white">
+      <div className={`text-${color}`}>
         <span>
           <i className="fa-solid fa-circle-info"></i>
         </span>
-        <span>알림창 꾸미기 + 아이콘 설정</span>
+        <span>{children}</span>
       </div>
     </div>
-  )
+  );
 }
 
 function App() {
   return (
     <>
-      <Alert />
+      <NotifyOnce>
+        <Alert>안녕하세요</Alert>
+      </NotifyOnce>
       <hr className="my-4" />
-      <Alert />
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Iusto quam accusamus esse voluptatem aliquam inventore amet 
+        consectetur labore blanditiis. Ipsa sed maiores non repudiandae 
+        cupiditate, dignissimos facere consequuntur cumque voluptatibus.
+      </div>
     </>
   );
 }
