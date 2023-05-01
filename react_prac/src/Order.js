20ded04d76ee135bb564ff29e8fe1fd122ecb76b
CHHyuk <gksgur1038@naver.com>
2023-05-01 20:11:18 +0900

    05/01 리액트 map 활용 체크리스트 만들기3

--- react_prac/src/Order.js
+++ react_prac/src/Order.js
@@ -6,32 +6,46 @@ export default function Order() {
     "랜치소스",
     "머스타드 소스",
     "마라 소스",
-    "스윗 칠리 소스"
+    "스윗 칠리 소스",
+    "레몬에이드"
   ];
-
+  
   const [optionCheckeds, setOptionCheckeds] = useState([
     false,
     true,
     false,
     false,
-    true
+    true,
+    true,
   ]);
 
   const toggleOptionCheck = (index) => {
     const newOptioncheckeds = optionCheckeds.map((el, _index) => _index == index ? !el : el);
-    setOptionCheckeds(newOptioncheckeds)
+    setOptionCheckeds(newOptioncheckeds);
   };
 
   const btnAllChecked = optionCheckeds.every((el) => el);
-  
+
+  const toggleAllChecked = () => {
+    if (btnAllChecked) {
+      const newOptioncheckeds = optionCheckeds.map((el) => false);
+      setOptionCheckeds(newOptioncheckeds);
+    }
+
+    else {
+      const newOptioncheckeds = optionCheckeds.map((el) => true);
+      setOptionCheckeds(newOptioncheckeds);
+    }
+  };
+
   return (
     <>
       <h1>음식 주문</h1>
       <h2>옵션</h2>
-      <span style={{ paddingLeft: 40 }}>{btnAllChecked ? "[v]" : "[ ]"} 전체선택</span>
-      <ul>
+      <span onClick={toggleAllChecked} style={{ paddingLeft: 40, userSelect: "none", cursor: "pointer" }}>{btnAllChecked ? "[v]" : "[ ]"}전체선택</span>
+      <ul style={{ paddingTop: 30 }}>
         {options.map((option, index) => (
-          <li style={{ cursor: "pointer", userSelect: "none" }} key={option} onClick={() => toggleOptionCheck(index)}>
+          <li style={{ cursor: "pointer", userSelect: "none", paddingLeft: 40 }} key={option} onClick={() => toggleOptionCheck(index)}>
             {optionCheckeds[index] ? "[v]" : "[ ]"}
             {option}
           </li>
