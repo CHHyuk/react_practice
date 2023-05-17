2c6d6ff5fb9fd8a3afce1fb981c940cf18fb593c
CHHyuk <gksgur1038@naver.com>
2023-05-17 15:58:04 +0900

    05/17 리액트 객체배열, id 1씩 증가, useState 이용

--- react_prac/src/App.js
+++ react_prac/src/App.js
@@ -1,41 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [lastTodoId, setLastTodoId] = useState(0);
 
-  const addTodo = (newTodo) => {
+  const addTodo = (newContent) => {
+    const id = lastTodoId + 1;
+    setLastTodoId(id);
+
+    const newTodo = {
+      id,
+      content: newContent,
+      regDate: "2023-05-17 15:51:02",
+    };
+
     const newTodos = [...todos, newTodo];
     setTodos(newTodos);
-  };
+  }
 
-  const removeTodo = (index) => {
-    const newTodos = todos.filter((_,_index) => _index != index);
-    setTodos(newTodos);
-  };
-
-  const modifyTodo = (index, newTodo) => {
-    const newTodos = todos.map((todo,_index) => _index != index ? todo : newTodo);
-    setTodos(newTodos);
-  };
-
-  const onAddBtnClick = () => {
-    addTodo(todos.length + 1);
-  };
-
-  const onRemoveBtnClick = () => {
-    removeTodo(1);    
-  };
-
-  const onEditBtnClick = () => {
-    modifyTodo(1, "안녕");    
+  const onBtnAddTodoClick = () => {
+    addTodo("안녕");
   };
 
   return (
     <>
-      <div>{JSON.stringify(todos)}</div>
-      <button onClick={onAddBtnClick}>추가</button>
-      <button onClick={onRemoveBtnClick}>삭제</button>
-      <button onClick={onEditBtnClick}>수정</button>
+      <button onClick={onBtnAddTodoClick}>추가</button>
+      <hr />
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo.id}
+            {todo.regDate}
+            {todo.content}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
