2e94aa71f720dc7991d15232997705f665a43232
CHHyuk <gksgur1038@naver.com>
2023-05-17 18:10:05 +0900

    05/17 리액트 컴포넌트 분리 / 응용 / 커스텀 훅

--- react_prac/src/App.js
+++ react_prac/src/App.js
@@ -1,32 +1,26 @@
 import React, { useState, useRef } from "react";
 
-function App() {
-  const [todos, setTodos] = useState([]);
-  const lastTodoIdRef = useRef(0);
-
-  const addTodo = (newContent) => {
-    const id = ++lastTodoIdRef.current;
-
-    const newTodo = {
-      id,
-      content: newContent,
-      regDate: "2023-05-17 15:51:02",
-    };
-
-    const newTodos = [...todos, newTodo];
-    setTodos(newTodos);
-  }
-
+function TodoApp({ todosState }) {
   const onBtnAddTodoClick = () => {
-    addTodo("안녕");
+    todosState.addTodo("안녕");
+  };
+
+  const onBtnRemoveTodoClick = () => {
+    todosState.removeTodo(1);
+  };
+
+  const onBtnModifyTodoClick = () => {
+    todosState.modifyTodo(1, "ㅋㅋㅋ");
   };
 
   return (
     <>
       <button onClick={onBtnAddTodoClick}>추가</button>
+      <button onClick={onBtnRemoveTodoClick}>삭제</button>
+      <button onClick={onBtnModifyTodoClick}>수정</button>
       <hr />
       <ul>
-        {todos.map((todo, index) => (
+        {todosState.todos.map((todo, index) => (
           <li key={index}>
             {todo.id}
             {todo.regDate}
@@ -34,6 +28,53 @@ function App() {
           </li>
         ))}
       </ul>
+    </>
+  );
+}
+
+function useTodosState() {
+  const [todos, setTodos] = useState([]);
+  const lastTodoIdRed = useRef(0);
+
+  const addTodo = (newContent) => {
+    const id = ++lastTodoIdRed.current;
+
+    const newTodo = {
+      id,
+      content: newContent,
+      regDate: "2023-05-03 12:12:12",
+    };
+
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, _index) => _index != index);
+    setTodos(newTodos);
+  };
+
+  const modifyTodo = (index, newContent) => {
+    const newTodos = todos.map((todo, _index) =>
+      _index != index ? todo : { ...todo, content: newContent }
+    );
+    setTodos(newTodos);
+  };
+
+  return {
+    todos,
+    addTodo,
+    modifyTodo,
+    removeTodo,
+  };
+}
+
+function App() {
+  const todosState = useTodosState();
+
+  return (
+    <>
+      <TodoApp todosState={todosState} />
     </>
   );
 }
