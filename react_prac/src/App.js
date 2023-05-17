9cd17f03e89e9ca791d95c2d4ae249a901e04a7d
CHHyuk <gksgur1038@naver.com>
2023-05-17 15:49:32 +0900

    05/17 리액트 배열 추가 삭제 수정

--- react_prac/src/App.js
+++ react_prac/src/App.js
@@ -6,20 +6,36 @@ function App() {
   const addTodo = (newTodo) => {
     const newTodos = [...todos, newTodo];
     setTodos(newTodos);
-  }
+  };
 
-  const onClick = () => {
-    // const netTodos = [...todos, todos.Length + 1];
-    // setTodos(newTodos);
-  
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_,_index) => _index != index);
+    setTodos(newTodos);
+  };
+
+  const modifyTodo = (index, newTodo) => {
+    const newTodos = todos.map((todo,_index) => _index != index ? todo : newTodo);
+    setTodos(newTodos);
+  };
+
+  const onAddBtnClick = () => {
     addTodo(todos.length + 1);
-  }
+  };
 
+  const onRemoveBtnClick = () => {
+    removeTodo(1);    
+  };
 
+  const onEditBtnClick = () => {
+    modifyTodo(1, "안녕");    
+  };
 
   return (
     <>
-      <button onClick={onClick}>{JSON.stringify(todos)}</button>
+      <div>{JSON.stringify(todos)}</div>
+      <button onClick={onAddBtnClick}>추가</button>
+      <button onClick={onRemoveBtnClick}>삭제</button>
+      <button onClick={onEditBtnClick}>수정</button>
     </>
   );
 }
